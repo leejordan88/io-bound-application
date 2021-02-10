1477b82aee00578187edd496bb3f4cfac0ccd6c8
joonsunglee <js.lee@petdoc.co.kr>
2021-02-10 12:29:52 +0900

    feat: rabbitmq 추가

--- src/main/java/class101/foo/io/PostController.java
+++ src/main/java/class101/foo/io/PostController.java
@@ -1,5 +1,8 @@
 package class101.foo.io;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
@@ -10,17 +13,20 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
+@RequiredArgsConstructor
 public class PostController {
 
     private static Integer PAGE_SIZE = 20;
-
-    @Autowired
-    PostRepository postRepository;
+    private final PostRepository postRepository;
+    private final Producer producer;
+    private final ObjectMapper objectMapper;
 
     // 1. 글을 작성한다.
     @PostMapping("/post")
-    public Post createPost(@RequestBody Post post) {
-        return postRepository.save(post);
+    public Post createPost(@RequestBody Post post) throws JsonProcessingException {
+        String jsonPost = objectMapper.writeValueAsString(post);
+        producer.sendTo(jsonPost);
+        return post;
     }
 
     // 2-1. 글 목록을 조회한다.
@@ -31,12 +37,12 @@ public class PostController {
         return postRepository.findAll(pageable);
     }
 
-    
     // 3. 글 번호로 조회
     @GetMapping("/post/{id}")
     public Post findById(@PathVariable("id") Long id) {
         return postRepository.findById(id).get();
     }
+
     // 4. 글 내용으로 검색 -> 해당 내용이 포함된 모든 글
     @GetMapping("/search")
     public List<Post> findPostByContent(@RequestParam String content) {
---
+++ src/main/java/class101/foo/io/Producer.java
@@ -0,0 +1,18 @@
+package class101.foo.io;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class Producer {
+
+    private final RabbitTemplate rabbitTemplate;
+    private static final String CREATE_POST_QUEUE = "CREATE_POST_QUEUE";
+
+    public void sendTo(String message) {
+        this.rabbitTemplate.convertAndSend(CREATE_POST_QUEUE, message);
+    }
+
+}
